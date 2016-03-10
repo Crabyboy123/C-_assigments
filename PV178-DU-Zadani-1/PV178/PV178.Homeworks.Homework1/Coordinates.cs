89e89476653b7746352d271b2098600360fec14d
Crabyboy123 <jakubikkristian@gmail.com>
2016-03-10 05:13:20 -0800

    First commit
    World class completed except tax method

---
+++ PV178-DU-Zadani-1/PV178/PV178.Homeworks.Homework1/Coordinates.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PV178.Homeworks.Homework1
+{
+    struct Coordinates : ICoordinates
+    {
+        public int left, top;
+        Coordinates(int left, int top)
+        {
+            this.left = left;
+            this.top = top;
+        }
+        
+        int Left
+        {
+            get
+            {
+                return left;
+            }
+            set
+            {
+                left = value;
+            }
+        }
+
+        int Top
+        {
+            get
+            {
+                return top;
+            }
+            set
+            {
+                top = value;
+            }
+        }
+
+        public bool IsInRectangle(int top, int left, int width, int height)
+        {
+            if (width <= 0)
+                throw new ArgumentOutOfRangeException("Width is in wrong range");
+
+            if (height <= 0) 
+                throw new ArgumentOutOfRangeException("Height is in wrong range");
+
+            if ((top + height >= this.top) && (left + width >= this.left))
+                return true;
+            return false;
+        }
+
+        public String toString()
+        {
+            return String.Format("[{0};{1}]", left, top);
+        }
+    }
+
+}
---
+++ PV178-DU-Zadani-1/PV178/PV178.Homeworks.Homework1/World.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PV178.Homeworks.Homework1
+{
+    class World : IWorld
+    {
+        private readonly int width;
+        private readonly int height;
+        private int freeTiles;
+        private Dictionary<ICoordinates, IBuilding> map;
+
+        World(int width, int height)
+        {
+            if (width <= 0)
+                throw new ArgumentException("Width is invalid");
+            if (height <= 0)
+                throw new ArgumentException("Height is invalid");
+
+            this.width = width;
+            this.height = height;
+            this.freeTiles = width * height;
+            this.map = new Dictionary<ICoordinates, IBuilding>;
+
+        }
+
+        int Width
+        {
+            get
+            {
+                return width;
+            }
+        }
+
+        int Height
+        {
+            get
+            {
+                return height;
+            }
+        }
+
+        int FreeTiles
+        {
+            get
+            {
+                return freeTiles;
+            }
+        }
+
+        ICoordinates GetBuildingLocation(IBuilding building)
+        {
+            if (building == null)
+                throw new ArgumentNullException("Building is null");
+
+            ICoordinates result = null;
+            result = map.FirstOrDefault(x => x.Value == building).Key;
+            if (result == null)
+                throw new InvalidOperationException("Building is not in map");
+
+            return result;
+        }
+
+        IBuilding GetBuildingAt(ICoordinates coordinates)
+        {
+            if (coordinates == null)
+                throw new ArgumentNullException("Coordinates are null");
+            if (!coordinates.IsInRectangle(0, 0, width, height))
+                throw new ArgumentOutOfRangeException("Coordinates are out of range");
+            IBuilding result = null;
+            try
+            {
+                result = map[coordinates];
+            }
+            catch (KeyNotFoundException)
+            {
+                return null;
+            }
+            return result;
+        }
+
+        void Build(ICoordinates coordinates, IBuilding building)
+        {
+            if (building == null)
+                throw new ArgumentNullException("Building is null");
+            if (coordinates == null)
+                throw new ArgumentNullException("Coordinates are null");
+            if (!coordinates.IsInRectangle(0, 0, width, height))
+                throw new ArgumentOutOfRangeException("Coordinates are out of range");
+            if (map.ContainsValue(building))
+                throw new InvalidOperationException("Building is already built");
+            if (map.ContainsKey(coordinates))
+                throw new InvalidOperationException("Coordinates are used");
+
+            map.Add(coordinates, building);
+            freeTiles--;
+        }
+
+        decimal CalculateLandTax()
+        {
+
+        }
+
+    }
+
+
+}
