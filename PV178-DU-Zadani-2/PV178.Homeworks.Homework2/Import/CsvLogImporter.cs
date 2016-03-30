13174ba2e36265b71a79870c0a6eb0a7d42a68d6
Crabyboy123 <jakubikkristian@gmail.com>
2016-03-30 06:59:58 -0700

    Dalsi commit

--- PV178-DU-Zadani-2/PV178.Homeworks.Homework2/Import/CsvLogImporter.cs
+++ PV178-DU-Zadani-2/PV178.Homeworks.Homework2/Import/CsvLogImporter.cs
@@ -1,6 +1,7 @@
 ﻿using PV178.Homeworks.Homework2.Storage;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,37 +10,53 @@ namespace PV178.Homeworks.Homework2.Import
 {
     public class CsvLogImporter<T> : ILogImporter<T> where T : class, new()
     {
-        private ILogStorage lS;
-        private string[] cM;
-        private bool hHR;
-        private char cD;
-        private char tQ;
+        private ILogStorage logStorage_;
+        private string[] columnMappings_;
+        private bool hasHeaderRow_;
+        private char columnDelimiter_;
+        private char textQualifier_;
 
         public CsvLogImporter(ILogStorage logStorage, string[] columnMappings, bool hasHeaderRow, char columnDelimiter, char textQualifier)
         {
             if (logStorage == null)
                 throw new ArgumentNullException("logStorage is null");
 
-            System.Reflection.MemberInfo info = typeof(T);
-            
-
-
-            lS = logStorage;
-            cM = columnMappings;
-            hHR = hasHeaderRow;
-            cD = columnDelimiter;
-            tQ = textQualifier;
+            logStorage_ = logStorage;
+            columnMappings_ = columnMappings;
+            hasHeaderRow_ = hasHeaderRow;
+            columnDelimiter_ = columnDelimiter;
+            textQualifier_ = textQualifier;
         }
 
         public List<T> Import()
         {
-            List<T> list = new List<T>();
-            string[] csv = lS.Load().Split(new string[]{Environment.NewLine}, StringSplitOptions.None);
-            foreach(string line in csv)
+            Type t = typeof(T);
+            PropertyInfo[] info = t.GetProperties();
+            foreach(var i in info)
             {
-
+                if (columnMappings_.Contains(i.Name))
+                    throw new ArgumentException("Property doesn't exist");
             }
 
+            string[] csv = logStorage_.Load().Split(new string[]{Environment.NewLine}, StringSplitOptions.None);
+            if (hasHeaderRow_)
+                Array.Copy(csv, 1, csv, 0, csv.Length - 1);
+
+            List<T> list = new List<T>();
+            foreach (string line in csv)
+            {
+                int i = 0;
+                T obj = new T();
+                string[] values = line.Split(columnDelimiter_);
+                foreach(var col in columnMappings_)
+                {
+                    var property = t.GetProperty(col);
+                    property.SetValue(obj, values[i]);
+                    i++;
+                }
+                list.Add(obj);
+            }
+            return list;
         }
     }
 }
--- PV178-DU-Zadani-2/PV178.Homeworks.Homework2/Import/JsonLogImporter.cs
+++ PV178-DU-Zadani-2/PV178.Homeworks.Homework2/Import/JsonLogImporter.cs
@@ -4,18 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace PV178.Homeworks.Homework2.Import
 {
     public class JsonLogImporter<T> : ILogImporter<T> where T : class, new()
     {
+        private ILogStorage logStorage_;
+
         public JsonLogImporter(ILogStorage logStorage)
         {
-
+            if (logStorage == null)
+                throw new ArgumentNullException("LogStorage is null");
         }
         public List<T> Import()
         {
-            throw new NotImplementedException();
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+            T obj = (T)serializer.ReadObject(logStorage_.Load());
+
         }
     }
 }
